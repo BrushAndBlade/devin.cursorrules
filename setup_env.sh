19e12add18edc7a7918013e89679b5bd9c49a784
BrushAndBlade <matthew@newschoolmarketing.io>
2025-02-16 18:37:02 -0600

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -5,7 +5,7 @@ set -x
 
 # Initialize environment
 if [ ! -d "venv" ]; then
-    /opt/homebrew/opt/python@3.9/bin/python3 -m venv venv --prompt "AI_Companion" --clear --verbose
+    python3 -m venv venv --prompt "AI_Companion" --clear
     source venv/bin/activate
     chmod +x venv/bin/activate
     pip install -U pip setuptools wheel
